2cd920481dc2673ff6aa6c5e334d59226a1ee638
Zach Goldberg <zach@zachgoldberg.com>
2011-11-01 01:14:36 -0400

    Implement CPU Usage restricting.
    As it turns out it is non trivial to get CPU usage, requires reading
    from both /proc/stat and /proc/PID/stat and doing a bunch of arithmatic

--- constraints.py
+++ constraints.py
@@ -21,7 +21,36 @@ class CPUConstraint(Constraint):
                                              cpu_limit)
 
     def CheckViolation(self, child_proc):
-        print "CPU Usage:"
+        if child_proc.UpdateUsage() and child_proc.last_usage:
+            proc_user_time_diff = (child_proc.usage[0] -
+                                   child_proc.last_usage[0])
+
+            kernel_user_time_diff = ((child_proc.system_usage[0] +
+                                      child_proc.system_usage[1]) -
+                                     (child_proc.last_system_usage[0] +
+                                      child_proc.last_system_usage[1])) or 1
+
+            proc_sys_time_diff = (child_proc.usage[2] -
+                                  child_proc.last_usage[2])
+
+            kernel_sys_time_diff = (child_proc.system_usage[2] -
+                                    child_proc.last_system_usage[2]) or 1
+
+            #print "kernel:", kernel_sys_time_diff, kernel_user_time_diff
+            #print "proc:", proc_sys_time_diff, proc_user_time_diff
+
+            user_time_perc = float(proc_user_time_diff) / kernel_user_time_diff
+            sys_time_perc = float(proc_sys_time_diff) / kernel_sys_time_diff
+
+            total_usage = user_time_perc + sys_time_perc
+
+            print total_usage, self.value
+
+            if total_usage > float(self.value):
+                print "CPU Limit Exceeded"
+                return True
+
+        return False
 
 
 class MemoryConstraint(Constraint):
@@ -31,4 +60,4 @@ class MemoryConstraint(Constraint):
                                              mem_limit)
 
     def CheckViolation(self, child_proc):
-        print "Memory Usage:"
+        child_proc.UpdateUsage()
--- process.py
+++ process.py
@@ -2,11 +2,41 @@
 A class to encapsulate data about a process
 """
 import os
+import signal
+import time
+
+
+def GetSystemCPUUsage():
+    """
+    Read out an array of info from /proc/stat
+    utime, nicetime, stime, idle, iowate, irq, softiq
+    """
+    cpuStats = file("/proc/stat", "r").readline()
+    columns = cpuStats.replace("cpu", "").split(" ")
+    return map(int, filter(None, columns))
+
+
+def GetProcCPUUsage(pid):
+    """
+    Read out an array of info from /proc/stat
+    utime, nicetime, stime, idle, iowate, irq, softiq
+    """
+    cpuStats = file("/proc/%d/stat" % pid, "r").readline()
+    columns = cpuStats.split(" ")
+    return map(int, [columns[13], 0, columns[14], 0, 0, 0, 0])
 
 
 class Process(object):
     def __init__(self, pid):
         self.pid = pid
+
+        self.previous_update_time = 0
+        self.last_usage_update = 0
+        self.usage = None
+        self.last_usage = None
+
+        self.system_usage = None
+        self.last_system_usage = None
 
     def IsAlive(self):
         try:
@@ -15,5 +45,29 @@ class Process(object):
         except OSError:
             return False
 
+    def ForceExit(self):
+        os.kill(self.pid, signal.SIGKILL)
+
     def WaitForCompletion(self):
         return os.waitpid(self.pid, 0)
+
+    def UpdateUsage(self):
+        """Update process usage.
+
+        Only updates at most once per 0.1 seconds
+
+        Return: True if we updated, otherwise False
+        """
+
+        now = time.time()
+        if now - self.last_usage_update > 0.1:
+            self.previous_update_time = self.last_usage_update
+            self.last_usage_update = now
+            self.last_usage = self.usage
+            self.usage = GetProcCPUUsage(self.pid)
+            self.last_system_usage = self.system_usage
+            self.system_usage = GetSystemCPUUsage()
+
+            return True
+
+        return False
