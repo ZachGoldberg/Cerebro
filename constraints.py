84fbef1debee3a0da0d8b4a55c3d7b473426b3ad
Zach Goldberg <zach@zachgoldberg.com>
2011-11-01 01:34:23 -0400

    Refactor CPU calculation to be in the proper place

--- constraints.py
+++ constraints.py
@@ -23,45 +23,9 @@ class CPUConstraint(Constraint):
     def CheckViolation(self, child_proc):
         """
         Calculate child CPU usage and see if it violates the constraint.
-
-        Side remark: Perhaps cpu usage calculations belong in process.py?
-
-        Reference: http://stackoverflow.com/questions/1420426/
-                   calculating-cpu-usage-of-a-process-in-linux
-
-        Formula:
-        utime_jiffys_proc_used / utime_jiffys_system_used +
-        stime_jiffys_proc_used / stime_jiffys_system_used
-
-        Return:
-          True if child is in violation, false otherwise
         """
-        if child_proc.UpdateUsage() and child_proc.last_usage:
-            proc_user_time_diff = (child_proc.usage[0] -
-                                   child_proc.last_usage[0])
-
-            system_user_time_diff = ((child_proc.system_usage[0] +
-                                      child_proc.system_usage[1]) -
-                                     (child_proc.last_system_usage[0] +
-                                      child_proc.last_system_usage[1])) or 1
-
-            proc_sys_time_diff = (child_proc.usage[2] -
-                                  child_proc.last_usage[2])
-
-            system_sys_time_diff = (child_proc.system_usage[2] -
-                                    child_proc.last_system_usage[2]) or 1
-
-            #print "system:", system_sys_time_diff, system_user_time_diff
-            #print "proc:", proc_sys_time_diff, proc_user_time_diff
-
-            user_time_perc = float(proc_user_time_diff) / system_user_time_diff
-            sys_time_perc = float(proc_sys_time_diff) / system_sys_time_diff
-
-            total_usage = user_time_perc + sys_time_perc
-
-            print total_usage, self.value
-
-            if total_usage > float(self.value):
+        if child_proc.UpdateUsage():
+            if child_proc.cpu_usage > float(self.value):
                 print "CPU Limit Exceeded"
                 return True
 
--- process.py
+++ process.py
@@ -6,26 +6,6 @@ import signal
 import time
 
 
-def GetSystemCPUUsage():
-    """
-    Read out an array of info from /proc/stat
-    utime, nicetime, stime, idle, iowate, irq, softiq
-    """
-    cpu_stats = file("/proc/stat", "r").readline()
-    columns = cpu_stats.replace("cpu", "").split(" ")
-    return map(int, filter(None, columns))
-
-
-def GetProcCPUUsage(pid):
-    """
-    Read out an array of info from /proc/pid/stat
-    utime, nicetime, stime, idle, iowate, irq, softiq
-    """
-    cpu_stats = file("/proc/%d/stat" % pid, "r").readline()
-    columns = cpu_stats.split(" ")
-    return map(int, [columns[13], 0, columns[14], 0, 0, 0, 0])
-
-
 class Process(object):
     def __init__(self, pid):
         self.pid = pid
@@ -34,9 +14,9 @@ class Process(object):
         self.last_usage_update = 0
         self.usage = None
         self.last_usage = None
-
         self.system_usage = None
         self.last_system_usage = None
+        self.cpu_usage = 0
 
     def IsAlive(self):
         try:
@@ -51,6 +31,67 @@ class Process(object):
     def WaitForCompletion(self):
         return os.waitpid(self.pid, 0)
 
+    @classmethod
+    def GetSystemCPUUsage(klass):
+        """
+        Read out an array of info from /proc/stat
+        utime, nicetime, stime, idle, iowate, irq, softiq
+        """
+
+        cpu_stats = file("/proc/stat", "r").readline()
+        columns = cpu_stats.replace("cpu", "").split(" ")
+        return map(int, filter(None, columns))
+
+    def GetProcCPUUsage(self):
+        """
+        Read out an array of info from /proc/pid/stat
+        utime, nicetime, stime, idle, iowate, irq, softiq
+        """
+        cpu_stats = file("/proc/%d/stat" % self.pid, "r").readline()
+        columns = cpu_stats.split(" ")
+        return map(int, [columns[13], 0, columns[14], 0, 0, 0, 0])
+
+    def CalculateCPUUsage(self):
+        """
+        Calculate CPU Usage for this process.
+
+        Reference: http://stackoverflow.com/questions/1420426/
+                   calculating-cpu-usage-of-a-process-in-linux
+
+        Formula:
+        utime_jiffys_proc_used / utime_jiffys_system_used +
+        stime_jiffys_proc_used / stime_jiffys_system_used
+
+        Return: True if child is in violation, false otherwise
+        """
+        if not self.usage or not self.last_usage:
+            return
+
+        proc_user_time_diff = (self.usage[0] -
+                               self.last_usage[0])
+
+        system_user_time_diff = ((self.system_usage[0] +
+                                  self.system_usage[1]) -
+                                 (self.last_system_usage[0] +
+                                  self.last_system_usage[1])) or 1
+
+        proc_sys_time_diff = (self.usage[2] -
+                              self.last_usage[2])
+
+        system_sys_time_diff = (self.system_usage[2] -
+                                self.last_system_usage[2]) or 1
+
+        #print "system:", system_sys_time_diff, system_user_time_diff
+        #print "proc:", proc_sys_time_diff, proc_user_time_diff
+
+        user_time_perc = float(proc_user_time_diff) / system_user_time_diff
+        sys_time_perc = float(proc_sys_time_diff) / system_sys_time_diff
+
+        self.cpu_usage = user_time_perc + sys_time_perc
+
+        #print self.cpu_usage
+        return self.cpu_usage
+
     def UpdateUsage(self):
         """Update process usage.
 
@@ -64,9 +105,11 @@ class Process(object):
             self.previous_update_time = self.last_usage_update
             self.last_usage_update = now
             self.last_usage = self.usage
-            self.usage = GetProcCPUUsage(self.pid)
+            self.usage = self.GetProcCPUUsage()
             self.last_system_usage = self.system_usage
-            self.system_usage = GetSystemCPUUsage()
+            self.system_usage = Process.GetSystemCPUUsage()
+
+            self.CalculateCPUUsage()
 
             return True
 
