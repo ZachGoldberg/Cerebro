7a648aeb5c94ff5a0b9f8cf78c876d9393c1f8f4
Zach Goldberg <zach@zachgoldberg.com>
2011-11-07 00:21:11 -0500

    Spacing, PEP8, and make things work in python 2.7 again

--- arg_parser.py
+++ arg_parser.py
@@ -1,27 +1,28 @@
 PARSER = "argparse"
 
+import optparse
+
 try:
     import argparse
 except ImportError:
     PARSER = "optparse"
-    import optparse
 
 
 class OptParserWrapper(optparse.OptionParser):
     def __init__(self, description=None, *args, **kwargs):
-        optparse.OptionParser.__init__(self, usage=description, *args, 
+        optparse.OptionParser.__init__(self, usage=description, *args,
                                      **kwargs)
 
-    def add_argument(self, name, dest=None, help=None, action=None, 
+    def add_argument(self, name, dest=None, help=None, action=None,
                      const=None, default=None, required=None):
         self.add_option(name, dest=dest, help=help, action=action,
                       default=default)
-        
+
     def parse_args(self, args):
         (options, args) = optparse.OptionParser.parse_args(self, args)
         return options
 
 if PARSER == "argparse":
-  ArgumentParser = argparse.ArgumentParser
+    ArgumentParser = argparse.ArgumentParser
 else:
-  ArgumentParser = OptParserWrapper
+    ArgumentParser = OptParserWrapper
