383065ff809b7e8d15c754c28740d856b07a20b2
Zach Goldberg <zach@wifast.com>
2012-03-12 11:50:46 -0700

    Save the clustersitterurl so we dont need to type it every time.

--- src/sittercommon/utils/main.py
+++ src/sittercommon/utils/main.py
@@ -3,6 +3,7 @@
 Cerebro client tool
 """
 import json
+import os
 import sys
 
 from sittercommon import arg_parser
@@ -20,11 +21,11 @@ def get_parser(default_options):
     parser = arg_parser.ArgumentParser(
         description="Cerebro Command Line Interaction Tool")
 
-    if not "clustersitter_url" in default_options:
-        parser.add_argument(
-            "--clustersitter-url", dest="clustersitter_url",
-            help="URL to the root of the clustersitter",
-            default=default_options.get('clustersitter_url'))
+
+    parser.add_argument(
+        "--clustersitter-url", dest="clustersitter_url",
+        help="URL to the root of the clustersitter",
+        default=default_options.get('clustersitter_url'))
 
     return parser
 
@@ -34,14 +35,16 @@ def main(sys_args=None):
         sys_args = sys.argv[1:]
 
     default_options = {}
+    CFG_FILE = os.path.expanduser("~/.cerebro.cfg")
+
     try:
-        default_data = open('~/.cerebro.cfg').read()
+        default_data = open(CFG_FILE).read()
         default_options = json.loads(default_data)
     except:
         pass
 
     command_parsers = {}
-
+    print default_options
     parser = get_parser(default_options)
     subparsers = parser.add_subparsers(help='Cerebro Command')
     for command in COMMANDS:
@@ -62,6 +65,11 @@ def main(sys_args=None):
     if not args.clustersitter_url:
         print "Please pass a clustersitter URL the first time you run cerebro"
         sys.exit(0)
+
+    default_options['clustersitter_url'] = args.clustersitter_url
+    default_file = open(CFG_FILE, 'w')
+    default_file.write(json.dumps(default_options))
+    default_file.close()
 
     for command in COMMANDS:
         if command.get_command() == args.command:
