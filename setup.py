ccf039b2098c6f78e4f5248bc0b3e2ef2ccdb451
Zach Goldberg <zach@wifast.com>
2012-03-13 01:03:31 -0700

    Version bump because this fix needs to go out ASAP

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cerebrod',
-      version='1.0.5',
+      version='1.0.6',
       description='A Cluster Management System',
       author='Zach Goldberg',
       author_email='zach@zachgoldberg.com',
