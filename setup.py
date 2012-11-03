038eb527fc72a2276c939aac247c859071453f1f
Zach Goldberg <zgoldberg@badass.(none)>
2012-11-03 14:36:24 -0700

    Ensure only one task is running at a time

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cerebrod',
-      version='1.1.15',
+      version='1.1.16',
       description='A Cluster Management System',
       author='Zach Goldberg',
       author_email='zach@zachgoldberg.com',
