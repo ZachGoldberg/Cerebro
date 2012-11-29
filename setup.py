350a039b5f680f915cd42b1889463b32fe07d560
Ryan Bourgeois <bluedragonx@gmail.com>
2012-11-28 18:08:00 -0800

    Version bump to v1.1.28 to fix hostname issues.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cerebrod',
-      version='1.1.28',
+      version='1.1.29',
       description='A Cluster Management System',
       author='Zach Goldberg',
       author_email='zach@zachgoldberg.com',
